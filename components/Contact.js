bdbba0df568913f445f12af24b2c2f12db0b7e36
Mariusz Borowiak <borowiakm85@gmail.com>
2018-06-21 21:33:11 +0100

    Change react elements syntax to JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,21 +5,20 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'http://icons.veryicon.com/png/System/nDroid/contacts%20blue%20glow.png'
-        }),
-        React.createElement('div',{className: 'contactInfo'},
-          React.createElement('p', {className: 'contactLabel'},
-            React.createElement('span', {className: 'bold'}, 'First name: '), this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'},
-            React.createElement('span', {className: 'bold'}, 'Last name: '), this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: `mailto:${this.props.item.email}`},
-            this.props.item.email
-          )
-        )
-      )
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/nDroid/contacts%20blue%20glow.png'} />
+        <div className={'contactInfo'}>
+          <p className={'contactLabel'}>
+            <span className={'bold'}>'First name: '</span>{this.props.item.firstName}
+          </p>
+          <p className={'contactLabel'}>
+            <span className={'bold'}>'Last name: '</span>{this.props.item.lastName}
+          </p>
+          <a className={'contactEmail'} href={`mailto:${this.props.item.email}`}>
+            {this.props.item.email}
+          </a>
+        </div>
+      </div>
     )
-  },
+  }
 });
--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 var Footer = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: 'footer'},
-      React.createElement(Counter),
-      React.createElement(Counter),
-      React.createElement(Counter)
-      )
+      <div className={'footer'}>
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
     )
   }
 });
